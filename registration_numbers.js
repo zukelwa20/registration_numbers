783ca9b18334597788b39d70f25e50e6b8f02e65
bootcamp <makhosandilezukelwa@gmail.com>
2017-03-14 13:46:56 +0200

    added registration plates

--- registration_numbers.js
+++ registration_numbers.js
@@ -1,1 +1,12 @@
-//var inp = document.getElementById("RegInput")
+var ul = document.getElementById("UL");
+
+function addFun() {
+
+var text = document.getElementById("inpBox").value;
+if(text.length>0 && text!==undefined){
+var li = document.createElement("li")
+li.textContent = text;
+ul.appendChild(li);
+}
+document.getElementById("inpBox").value="";
+};
