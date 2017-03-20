cab5d5236732c4a8f770343bf0f27b9f3c19dea3
bootcamp <makhosandilezukelwa@gmail.com>
2017-03-20 15:28:00 +0200

    my registration webpage

--- registration_numbers.js
+++ registration_numbers.js
@@ -1,12 +1,40 @@
 var ul = document.getElementById("UL");
+var optBtn = document.querySelector(".optBtn");
+
+var idOne = document.getElementById("capetown");
+var idTwo = document.getElementById("belville");
+var idThree = document.getElementById("paarl");
+var idFour = document.getElementById("all");
+var Registration = document.getElementsByName("Registration");
+var regText = document.getElementById("inpBox").value;
+
 
 function addFun() {
 
-var text = document.getElementById("inpBox").value;
-if(text.length>0 && text!==undefined){
-var li = document.createElement("li")
-li.textContent = text;
-ul.appendChild(li);
-}
-document.getElementById("inpBox").value="";
+
+    var regText = document.getElementById("inpBox").value;
+    if (regText.length > 0 && regText !== undefined) {
+        var li = document.createElement("li")
+        li.textContent = regText;
+
+        ul.appendChild(li);
+    }
+    document.getElementById("inpBox").value = "";
 };
+
+
+optBtn.addEventListener("change", function(e) {
+    var registrationNum = document.querySelectorAll("li");
+
+    for (var i = 0; i < registrationNum.length; i++) {
+        var text = registrationNum[i].innerHTML;
+
+        if (e.target.value === "All") {
+            registrationNum[i].style.display = "block";
+        } else if (text.startsWith(e.target.value)) {
+            registrationNum[i].style.display = "block";
+        } else {
+            registrationNum[i].style.display = "none";
+        }
+    }
+});
